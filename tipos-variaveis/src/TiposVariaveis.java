297e7839075b79ac114567fdb41338297d39539f
AlissoNNunes1 <alissonandradennnunes@gmail.com>
2024-03-08 11:12:30 -0300

    Changed variable types to byte, short, int, long, float, and double. Added a constant variable VALOR_PI.

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,20 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        byte idade = 123;
+        short ano  = 2021;
+        int cep = 49100000; // se começar com zero,talvez tenha que ser outro tipo
+        long cpf = 374620652L; // se começar com zero,talvez tenha que ser outro tipo
+        float pi = 3.14F; // float 
+        double preco = 8.9;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+
+        final double VALOR_PI = 3.14;
+
+        //VALOR_PI = 23.14; // não podemos alterar o valor de constante
+        System.out.println("Idade: " + idade);
     }
 }
